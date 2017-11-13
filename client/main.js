14fb5df69977c7a7f52054da7ab131e02bde91a8
kamran134 <kuzya134@gmail.com>
2017-11-14 02:00:04 +0400

    scroll

--- client/main.js
+++ client/main.js
@@ -3,3 +3,63 @@ import { ReactiveVar } from 'meteor/reactive-var';
 import { Router, RouteController }	from 'meteor/iron:router';
 
 import './main.html';
+
+Template.main.onRendered(function () {
+    'use strict';
+    this.autorun(function () {
+        $('.eventblock').addClass('show-blk');
+        $('.content-bet-info').addClass('show-blk');
+
+
+        (function ($) {
+            $("#tabs").tabs();
+        });
+
+        (function ($) {
+            $('.datepicker').datepicker();
+        });
+
+        jQuery(function ($) {
+            $.datepicker.regional['ru'] = {
+                closeText: 'Закрыть',
+                prevText: 'Назад',
+                nextText: 'Далее',
+                monthNames: ['Январь', 'Февраль', 'Март', 'Апрель', 'Май', 'Июнь', 'Июль', 'Август', 'Сентябрь', 'Октябрь', 'Ноябрь', 'Декабрь'],
+                monthNamesShort: ['Янв', 'Фев', 'Мар', 'Апр', 'Май', 'Июн', 'Июл', 'Авг', 'Сен', 'Окт', 'Ноя', 'Дек'],
+                dayNamesMin: ['Вс', 'Пн', 'Вт', 'Ср', 'Чт', 'Пт', 'Сб'],
+                dateFormat: 'dd.mm.yy',
+                firstDay: 1,
+                isRTL: false,
+                showMonthAfterYear: false,
+                yearSuffix: ''
+            };
+            $.datepicker.setDefaults($.datepicker.regional['ru']);
+        });
+
+        (function ($) {
+            $("#accordion").accordion({
+                heightStyle: "content"
+            });
+        });
+
+        $('#basic_example_2').timepicker({
+            timeOnlyTitle: 'Выберите время',
+            timeText: 'Время',
+            hourText: 'Часы',
+            minuteText: 'Минуты',
+            secondText: 'Секунды',
+            currentText: 'Сейчас',
+            closeText: 'Закрыть'
+        });
+
+        $('#basic_example_3').timepicker({
+            timeOnlyTitle: 'Выберите время',
+            timeText: 'Время',
+            hourText: 'Часы',
+            minuteText: 'Минуты',
+            secondText: 'Секунды',
+            currentText: 'Сейчас',
+            closeText: 'Закрыть'
+        });
+    });
+})
